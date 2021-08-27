c460bec857c34ce38ddedf4ecb2f012b7ce7f31e
qdchongMom <chong_qi_da@mom.gov.sg>
2021-08-27 12:26:42 +0800

    add update and delete route

--- routes/pokemon.route.js
+++ routes/pokemon.route.js
@@ -15,8 +15,12 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
-    const pokemons = await db.Pokemon.findByPk(req.params.id);
-    res.status(200).json(pokemons);
+    const pokemon = await db.Pokemon.findByPk(req.params.id);
+    if (pokemon === null) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(pokemon);
+    }
   } catch {
     error;
     next(error);
@@ -26,7 +30,40 @@ router.get("/:id", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   try {
     const pokemon = await db.Pokemon.create(req.body);
-    res.status(201).json(created.toJSON());
+    res.status(201).json(pokemon.toJSON());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/update/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const reqBody = req.body;
+
+    const [numOfUpdatedRecord, updatedRecord] = await db.Pokemon.update(
+      reqBody,
+      {
+        where: {
+          id: id,
+        },
+        returning: true,
+      }
+    );
+    res.json({
+      message: `Updated ${numOfUpdatedRecord} records successfully!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedPokemon = await db.Pokemon.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json({ deletedRecordsCount: deletedPokemon });
   } catch (error) {
     next(error);
   }
