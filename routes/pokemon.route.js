c25e49845dcd45e940d038553e6c7799029fd2da
qdchongMom <chong_qi_da@mom.gov.sg>
2021-08-27 11:31:20 +0800

    added get by id and post routes

--- routes/pokemon.route.js
+++ routes/pokemon.route.js
@@ -13,4 +13,22 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const pokemons = await db.Pokemon.findByPk(req.params.id);
+    res.status(200).json(pokemons);
+  } catch {
+    error;
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const pokemon = await db.Pokemon.create(req.body);
+    res.status(201).json(created.toJSON());
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
